fdb02fe29bdc3acc2b5a7a40136592a0f9b8e6ce
sfontes94 <sfontes94@gmail.com>
2023-05-13 02:48:16 +0100

    hamburger menu on process | loader complete

--- src/index.js
+++ src/index.js
@@ -108,3 +108,17 @@ function sendEmail() {
 }
 
 document.querySelector('.btn').addEventListener('click', sendEmail);
+
+
+/*-------------------SUBMIT EMAIL------------------*/
+document.onreadystatechange = function() {
+  
+  if (document.readyState !== "complete") {
+    document.querySelector("body").style.visibility = "hidden";
+    document.querySelector("#loader").style.visibility = "visible";
+
+  } else {
+    document.querySelector("#loader").style.display = "none";
+    document.querySelector("body").style.visibility = "visible";
+  }
+};
